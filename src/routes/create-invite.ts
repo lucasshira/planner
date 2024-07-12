8cef4e429e6c01a02d40145bc66207aab008afad
Lucas Gabriel <lucas.shirabayashi@gmail.com>
2024-07-12 17:03:58 -0300

    feat: create update trip route

---
+++ src/routes/create-invite.ts
@@ -0,0 +1,73 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from 'zod';
+import { prisma } from "../lib/prisma";
+import { dayjs } from "../lib/dayjs";
+import { getMailClient } from "../lib/mail";
+import nodemailer from 'nodemailer';
+
+export async function createInvite(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post('/trips/:tripId/invites', {
+    schema: {
+        params: z.object({
+          tripId: z.string().uuid(),
+        }),
+        body: z.object({
+          email: z.string().email(),
+        }),
+      },
+    },
+    async (request) => {
+      const { tripId } = request.params
+      const { email } = request.body
+
+      const trip = await prisma.trip.findUnique({
+        where: { id: tripId }
+      })
+
+      if (!trip) {
+        throw new Error('Trip not found.')
+      }
+
+      const participant = await prisma.participant.create({
+        data: {
+          email,
+          trip_id: tripId,
+        }
+      })
+
+      const formattedStartDate = dayjs(trip.starts_at).format('LL');
+      const formattedEndDate = dayjs(trip.ends_at).format('LL');
+
+      const mail = await getMailClient()
+
+      const confirmationLink = `http://localhost:3333/participants/${participant.id}/confirm/`;
+
+      const message = await mail.sendMail({
+        from: {
+          name: 'Equipe plann.er',
+          address: 'oi@planner.com',
+        },
+        to: participant.email,
+        subject: `Confirme sua presença na viagem para ${trip.destination} em ${formattedStartDate}`,
+        html: `
+          <div style="font-family: sans-serif; font-size: 16px; line-height: 1.6;">
+            <p>Você foi convidado(a) para participar de uma viagem para <strong>${trip.destination}</strong> nas datas de <strong>${formattedStartDate}</strong> ate <strong>${formattedEndDate}</strong>.</p>
+            <p></p>
+            <p>Para confirmar sua presença na viagem, clique no link abaixo:</p>
+            <p></p>
+            <p>
+              <a href="${confirmationLink}">Confirmar viagem:</a>
+            </p>
+            <p></p>
+            <p>Caso você não saiba do que se trata este e-mail, favor ignorar.</p>
+          </div>
+        `.trim()
+      })
+
+      console.log(nodemailer.getTestMessageUrl(message))
+
+      return { participantId: participant.id }
+    },
+  )
+}
--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,9 @@ import { createActivity } from "./routes/create-activity";
 import { getActivities } from "./routes/get-activities";
 import { createLink } from "./routes/create-link";
 import { getLinks } from "./routes/get-links";
+import { getParticipants } from "./routes/get-participants";
+import { createInvite } from "./routes/create-invite";
+import { updateTrip } from "./routes/update-trip";
 
 const app = fastify();
 
@@ -27,6 +30,9 @@ app.register(createActivity);
 app.register(getActivities);
 app.register(createLink);
 app.register(getLinks);
+app.register(getParticipants);
+app.register(createInvite);
+app.register(updateTrip);
 
 app.listen({ port: 3333 }).then(() => {
   console.log(`Server running!`)
