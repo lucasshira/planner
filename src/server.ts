1c81d17452599ab50261aa494b55410202100092
Lucas Gabriel <lucas.shirabayashi@gmail.com>
2024-07-12 15:26:12 -0300

    feat: create get links and get activities route

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,9 @@ import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod
 import { confirmTrip } from "./routes/confirm-trip";
 import { confirmParticipant } from "./routes/confirm-participant";
 import { createActivity } from "./routes/create-activity";
+import { getActivities } from "./routes/get-activities";
+import { createLink } from "./routes/create-link";
+import { getLinks } from "./routes/get-links";
 
 const app = fastify();
 
@@ -21,6 +24,9 @@ app.register(createTrip);
 app.register(confirmTrip);
 app.register(confirmParticipant);
 app.register(createActivity);
+app.register(getActivities);
+app.register(createLink);
+app.register(getLinks);
 
 app.listen({ port: 3333 }).then(() => {
   console.log(`Server running!`)
