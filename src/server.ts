d1cf6f291759d0a96b48a24cf509883dddd27c10
Lucas Gabriel <lucas.shirabayashi@gmail.com>
2024-07-12 12:19:44 -0300

    feat: create activities route

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { createTrip } from "./routes/create-trip";
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 import { confirmTrip } from "./routes/confirm-trip";
 import { confirmParticipant } from "./routes/confirm-participant";
+import { createActivity } from "./routes/create-activity";
 
 const app = fastify();
 
@@ -19,6 +20,7 @@ app.setSerializerCompiler(serializerCompiler);
 app.register(createTrip);
 app.register(confirmTrip);
 app.register(confirmParticipant);
+app.register(createActivity);
 
 app.listen({ port: 3333 }).then(() => {
   console.log(`Server running!`)
